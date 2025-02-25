68178909251be0787cb2a4f5bef4798588e0f5e4
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2025-02-25 11:45:12 +0700

    ultra refactor

--- src/features/home/home/components/input-thread.tsx
+++ src/features/home/home/components/input-thread.tsx
@@ -1,18 +1,29 @@
 import {
   Box,
   Button,
+  DialogBackdrop,
   Flex,
   Float,
   Image,
   Input,
   Spinner,
   Stack,
+  Textarea,
 } from "@chakra-ui/react";
 import { Avatar } from "@/components/ui/avatar.tsx";
 import useInputThread from "../hooks/useInputThread";
 import { CloseButton } from "@/components/ui/close-button";
 import { useAuthStore } from "@/store/useAuth";
 import InputContent from "./input";
+import { useState } from "react";
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogRoot,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function InputThread() {
   const {
@@ -30,6 +41,7 @@ export default function InputThread() {
     setValue,
   } = useInputThread();
   const userLogin = useAuthStore((state) => state.user);
+  const [isOpen, setOpen] = useState<boolean>(false);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -43,9 +55,106 @@ export default function InputThread() {
             size="xl"
           />
 
-          <Flex w="100vw">
-            <InputContent register={register} />
-          </Flex>
+          <DialogRoot
+            size={"lg"}
+            open={isOpen}
+            onOpenChange={(details) => setOpen(details.open)}
+          >
+            <DialogTrigger asChild>
+              <Flex w="100vw">
+                <InputContent register={register} />
+              </Flex>
+            </DialogTrigger>
+            <DialogBackdrop />
+            <DialogContent rounded="lg">
+              <DialogCloseTrigger bg="bg" />
+              <DialogBody p="5">
+                <Flex justify="center" align="start" flexShrink={0}>
+                  <Box p="2.5">
+                    <Avatar
+                      alignSelf="center"
+                      src="./src/assets/avatar2.svg"
+                      size="md"
+                    />
+                  </Box>
+                  <Textarea
+                    autoresize
+                    maxH="30vh"
+                    {...register("content")}
+                    placeholder="What is Happening?!"
+                    variant="flushed"
+                    p="4"
+                    textStyle="lg"
+                    minW="xs"
+                    borderBottom="none"
+                    _focus={{ borderBottom: "none", boxShadow: "none" }}
+                  />
+                </Flex>
+              </DialogBody>
+              <DialogFooter justifyContent="space-between">
+                <Flex alignSelf="start" as="label" cursor="pointer">
+                  <Image src="./src/assets/gallery-add.svg" />
+                  <input
+                    type="file"
+                    hidden
+                    {...restRegisterImages}
+                    onChange={(e) => {
+                      handlePreview(e);
+                      registerImagesOnChange(e);
+                    }}
+                    ref={(e) => {
+                      registerImagesRef(e);
+                      inputFileRef.current = e;
+                    }}
+                  />
+                </Flex>
+                <Button
+                  bgColor="#04A51E"
+                  color="white"
+                  type="submit"
+                  rounded="full"
+                  p="4"
+                  onClick={async () => {
+                    await handleSubmit(async (data) => {
+                      await onSubmit(data);
+                      setOpen(false);
+                    })();
+                  }}
+                  disabled={isPending ? true : false}
+                >
+                  {isPending ? <Spinner /> : "Post"}
+                </Button>
+                <Stack
+                  w="xs"
+                  alignSelf="center"
+                  p="2"
+                  position={"relative"}
+                  display={previewURL ? "flex" : "none"}
+                >
+                  <Image
+                    objectFit="contain"
+                    maxHeight="300px"
+                    maxWidth="300px"
+                    src={previewURL ?? ""}
+                    borderRadius="md"
+                  />
+                  {previewURL && (
+                    <Float>
+                      <CloseButton
+                        onClick={() => {
+                          setPreviewURL(null);
+                          setValue("imageContent", new DataTransfer().files);
+                        }}
+                        variant={"solid"}
+                        rounded={"full"}
+                        size={"xs"}
+                      />
+                    </Float>
+                  )}
+                </Stack>
+              </DialogFooter>
+            </DialogContent>
+          </DialogRoot>
 
           <Box as="label" cursor="pointer" display={"flex"}>
             <Avatar
@@ -71,7 +180,7 @@ export default function InputThread() {
               />
             </Avatar>
           </Box>
-          
+
           <Button
             type="submit"
             size="md"
