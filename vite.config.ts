9948e3440fe5520907df3dd88b9a1b5df6102321
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2025-02-26 17:23:41 +0700

    aaaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-})
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ["circle.alfiandirizki.com"],
+  },
+});
